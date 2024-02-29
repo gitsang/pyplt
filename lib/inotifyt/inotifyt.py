5af5c0bf9fc361338a0b1e82443fa07dd3062fa4
sang <sang.chen@outlook.com>
2024-02-29 16:36:44 +0800

    feat: update logs

--- lib/inotifyt/inotifyt.py
+++ lib/inotifyt/inotifyt.py
@@ -2,7 +2,7 @@
 # import inotify.adapters
 import os
 import logging
-from inotify_simple import INotify, masks
+from inotify_simple import INotify, flags, masks
 
 logging.basicConfig(level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -18,7 +18,7 @@ def watch(path):
     while 1:
         logger.info("waiting for events in %s", path)
         for events in i.read():
-            logger.info("got events %s in %s", events, path)
+            logger.info("got events %s in %s", flags.from_mask(events[1]), path)
 
 if __name__ == "__main__":
     PATH = "./logs/inotify.log"
